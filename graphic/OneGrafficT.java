53b6e9161e8ca72d7ac8a5280e0c2152d093a667
Reb Sena <r3b3casena@gmail.com>
2025-11-13 23:27:10 -0300

    Add files via upload

--- graphic/OneGrafficT.java
+++ graphic/OneGrafficT.java
@@ -1,1 +1,18 @@
+import javax.swing.*;
 
+public class OneGraffic {
+
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Graffica Beca");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+
+        ImageIcon image = new ImageIcon("images-removebg-preview.png");
+        frame.setIconImage(image.getImage());
+    }
+}
